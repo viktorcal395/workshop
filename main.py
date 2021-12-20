e860a6ee9ab73ee2af523bb5bd1cd067f76de16f
viktorcal395 <viktorcal395@gmail.com>
2021-12-20 10:00:50 -0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "Hello world from US"
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
