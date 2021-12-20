1f102dbaa0a3e8fddee9a3f12a2074534dcf9305
viktorcal395 <viktorcal395@gmail.com>
2021-12-20 09:38:11 -0600

    Create index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  const name = process.env.NAME || 'Pasteles';
+  res.send(`Hello ${name}!`);
+});
+
+const port = process.env.PORT || 8080;
+
+app.listen(port, () => {
+  console.log(`helloworld: listening on port ${port}`);
+});
+
+module.exports = app;
+© 2021 GitHub, Inc.
+Terms
